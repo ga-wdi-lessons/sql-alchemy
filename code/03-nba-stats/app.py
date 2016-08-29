96a8659b97bd4655993a9f1a2887bf9ea8ec654e
Jesse Shawl <jesse@jshawl.com>
2016-08-29 11:07:32 -0400

    rm solution

--- code/03-nba-stats/app.py
+++ code/03-nba-stats/app.py
@@ -1,33 +1,8 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 import csv
-
-Base = declarative_base()
-engine = create_engine('sqlite:///player.db')
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
-class Player(Base):
-  __tablename__ = 'player'
-  id = Column(Integer, primary_key=True)
-  name = Column(String(250))
-  age = Column(Integer)
-  team = Column(String(250))
-  game = Column(Integer)
-  points = Column(Integer)
-
-  def __repr__(self):
-    return "Player(id=%r,name=%r,age=%r,team=%r,game=%r,points=%r)" % (self.id,self.name, self.age, self.team, self.game, self.points)
-
-Base.metadata.create_all(engine)
-
 with open('data.csv') as f:
     reader = csv.reader(f)
     for row in reader:
-	session.add(Player(name=row[0],age=row[1],team=row[2],game=row[3],points=row[4]))
-	session.commit()
+        print row
 
 import code; code.interact(local=dict(globals(), **locals()))
 
--- code/03-nba-stats/readme.md
+++ code/03-nba-stats/readme.md
@@ -37,27 +37,15 @@ It may help to think about the SQL being executed as well:
 
 2. All columns for all players from the Indiana Packers (IND) who are under 26 years old
 
-> `session.query(Player).filter_by(team='IND').filter(Player.age < 26).all()`
-
 3. All columns for all players, ordered from least points scored to most points scored
-
-> `session.query(Player).order_by(Player.points).all()`
 
 4. Players with the top 20 points per game
 
-> `session.query(Player).order_by("points desc").limit(20).all()`
-
 5. The average age for all players
-
-> `session.query(func.avg(Player.age))`
 
 6. The average age for all players on the Oklahoma City Thunder (OKC)
 
-> `session.query(func.avg(Player.age)).filter_by(team='OKC').all()`
-
 7. The average age for all players who played more than 40 games
-
-> `session.query(func.avg(Player.age)).filter(Player.game > 40).all()`
 
 #### Bonus
 
